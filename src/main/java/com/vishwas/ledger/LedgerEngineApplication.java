86addbc2796ca7a82a3f145c59dee33527999a5a
Vishwas <vishwasvishu555@gmail.com>
2026-05-25 10:41:51 +0530

    config: configure Spring Boot application entries, H2 test profiles, and properties

---
+++ src/main/java/com/vishwas/ledger/LedgerEngineApplication.java
@@ -0,0 +1,12 @@
+package com.vishwas.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerEngineApplication.class, args);
+    }
+}
